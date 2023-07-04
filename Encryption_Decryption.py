fbb30f8bd7671bee5ccfcfa8b404ab00c2ca24b1
Mohsin Shahzad <115651091+33Mohsin33@users.noreply.github.com>
2023-07-04 18:52:50 +0500

    Add files via upload

---
+++ Encryption_Decryption.py
@@ -0,0 +1,76 @@
+import random
+Encode={
+          'a': '!',
+          'b': '@',
+          'c': '#',
+          'd': '$',
+          'e': '%',
+          'f': '^',
+          'g': '&',
+          'h': ':',
+          'i': ')',
+          'j': '(',
+          'k': '/',
+          'l': '-',
+          'm': '=',
+          'n': '+',
+          'o': '*',
+          'p': '|',
+          'q': '[',
+          'r': ']',
+          's': '{',
+          't': '}',
+          'u': '_', 
+          'v': '`',
+          'w': '~',
+          'x': ' ',
+          'y': '3',
+          'z': '5',
+          'A': '+!',
+          'B': '+@',
+          'C': '+#',
+          'D': '+$',
+          'E': '+%',
+          'F': '+^',
+          'G': '+&',
+          'H': '+:',
+          'I': '+)',
+          'J': '+(',
+          'K': '+/',
+          'L': '+-',
+          'M': '+=',
+          'N': '++',
+          'O': '+*',
+          'P': '+|',
+          'Q': '+[',
+          'R': '+]',
+          'S': '+{',
+          'T': '+}',
+          'U': '+_', 
+          'V': '+`',
+          'W': '+~',
+          'X': '+ ',
+          'Y': '-3',
+          'Z': '-5',
+          ' ': 'SCP'
+          }
+Decode= {value: key for key, value in Encode.items()}
+asciii=[]
+j=0
+inp=input("Enter Any Paragraph To Encrypt it (Only_Alphabets)\n")
+for i in range(0,len(inp)):
+    ch=Encode[inp[i]]
+    asciii.append(ch)
+    random_key = random.choice(list(Encode.values()))
+    asciii.append(random_key)
+    random_key = random.choice(list(Encode.values()))
+    asciii.append(random_key)
+    random_key = random.choice(list(Encode.values()))
+    asciii.append(random_key)
+dec=[]
+print("Encoded : ",asciii) 
+for i in range(0,len(asciii),4) :
+ ch=Decode[asciii[i]]
+ dec.append(ch)
+print("Decoded : ",dec)
+ 
